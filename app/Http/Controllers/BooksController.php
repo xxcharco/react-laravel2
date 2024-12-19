e1b85d1181c3ec3e566f7b4464b405c4900a4d44
xxcharco <xxcharco@gmail.com>
2024-12-19 22:45:18 +0900

    追加: コントローラーとルートの設定

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Books;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class BooksController extends Controller
 {
@@ -12,7 +13,14 @@ class BooksController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::find(auth()->id());
+        $books = Books::all();
+        return Inertia::render('Books/Index', [
+            'books' => $books,
+            'user_id' => $user->id,
+            'user_name' => $user->name,
+            'message' => session('message')
+        ]);
     }
 
     /**
@@ -28,7 +36,16 @@ class BooksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:100',
+            'category' => 'required|max:10',
+        ]);
+        $book = new Books($request->input());
+        $book->save();
+        return redirect('books')->with([
+            'message' => '登録しました',
+        ]);
     }
 
     /**
@@ -50,16 +67,24 @@ class BooksController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Books $books)
+    public function update(Request $request, $id)
     {
-        //
+        $book = Books::find($id);
+        $book->fill($request->input())->saveOrFail();
+        return redirect('books')->with([
+            'message' => '更新しました',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Books $books)
+    public function destroy($id)
     {
-        //
+        $book = Books::find($id);
+        $book->delete();
+        return redirect('books')->with([
+            'message' => '削除しました',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BooksController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+     // Books関連
+     Route::get('/books', [BooksController::class, 'index'])->name('books.index');
+     Route::post('/books/store', [BooksController::class, 'store'])->name('books.store');
+     Route::put('/books/update/{id}', [BooksController::class, 'update'])->name('books.update');
+     Route::delete('/books/destroy/{id}', [BooksController::class, 'destroy'])->name('books.destroy');
 });
 
 require __DIR__.'/auth.php';
